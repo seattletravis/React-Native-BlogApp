04420094045742c9374bd7525cf94c47d03f3941
Travis Lamberte <travis@lamberte.com>
2023-11-07 08:21:58 -0800

    add useState to manage blogposts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,13 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const BlogContext = React.createContext()
 
 export const BlogProvider = ({ children }) => {
-  const blogPosts = [
-    { title: 'Blog Post #1'},
-    { title: 'Blog Post #2'},
-  ]
-
+  const [blogPosts, setBlogPosts] = useState([])
 
     return <BlogContext.Provider value={blogPosts}>
         {children}
