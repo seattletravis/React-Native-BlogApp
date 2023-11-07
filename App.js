1c8f215ddd392ff020f007aa9593f242f44a238e
Travis Lamberte <travis@lamberte.com>
2023-11-07 06:54:45 -0800

    wrap <App /> in BlogProvider

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import { createAppContainer } from 'react-navigation'
 import {createStackNavigator } from 'react-navigation-stack'
-
 import IndexScreen from './src/screens/IndexScreen'
+import { BlogProvider } from './src/context/BlogContext'
 
 const navigator = createStackNavigator({
   Index: IndexScreen
-},{
-  initialRouteName: 'Index',
-  defaultNavigationOptions: {
-    title: 'Blogs'
+  },
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs'
+    }
   }
-
-})
+)
 
 const App = createAppContainer(navigator)
 
 export default () => {
-  return <App />
+  return <BlogProvider>
+      <App />
+    </BlogProvider>
 }
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BlogContext = React.createContext()
 
-const BlogProvider = ({ children }) => {
+export const BlogProvider = ({ children }) => {
     return <BlogContext.Provider>
         {children}
     </BlogContext.Provider>
