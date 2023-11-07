26e756e362fc8a64fb8c01947877223ec187d069
Travis Lamberte <travis@lamberte.com>
2023-11-07 06:33:13 -0800

    change App to return <App />

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { createAppContainer } from 'react-navigation'
 import {createStackNavigator } from 'react-navigation-stack'
 
@@ -13,4 +14,8 @@ const navigator = createStackNavigator({
 
 })
 
-export default createAppContainer(navigator)
+const App = createAppContainer(navigator)
+
+export default () => {
+  return <App />
+}
