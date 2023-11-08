da09ba78cc236e0e264479d8f2fd720cf2e01e42
Travis Lamberte <travis@lamberte.com>
2023-11-08 12:20:46 -0800

    provide context to showscreen

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { Context } from '../context/BlogContext'
 
-const ShowScreen = () => {
-    return (
-        <View>
-            <Text>Show Blog Screen</Text>
-        </View>
-    )
+const ShowScreen = ({ navigation }) => {
+  const { state } = useContext(Context)
+
+  const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'))
+
+  return (
+    <View>
+      <Text>{blogPost.title}</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
