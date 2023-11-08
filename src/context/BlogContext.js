9c1e793267eb868e87402245a0df1f442c2376ce
Travis Lamberte <travis@lamberte.com>
2023-11-08 11:56:45 -0800

    add trash functinality to icons

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,7 +2,9 @@ import createDataContext from './createDataContext'
 
 const blogReducer = (state, action) => {
   switch (action.type) {
-    case 'add_blogPost': 
+    case 'delete_blogpost':
+      return state.filter((blogPost) => blogPost.id !== action.payload)
+    case 'add_blogpost': 
       return [...state, { id: Math.floor(Math.random() * 99999), title: `Blog Post #${state.length + 1}` }]
     default:
       return state
@@ -11,14 +13,18 @@ const blogReducer = (state, action) => {
 
 const addBlogPost = dispatch => {
   return () => {
-    dispatch({ type: 'add_blogPost' })
+    dispatch({ type: 'add_blogpost' })
   }
 }
 
-
+const deleteBlogPost = dispatch => {
+  return id => {
+    dispatch({ type: 'delete_blogpost', payload: id })
+  }
+}
 
 export const { Context, Provider } = createDataContext(
   blogReducer, 
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   []
 )
