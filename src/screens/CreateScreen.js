87aa16947b12e7b46d0a28667ad9f685b41adca0
Travis Lamberte <travis@lamberte.com>
2023-11-09 08:54:06 -0800

    fix state issue

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { View, Text, TextInput, StyleSheet } from 'react-native'
 import { Context } from '../context/BlogContext'
 
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -30,7 +30,7 @@ const IndexScreen = ({ navigation }) => {
   )
 }
 
-IndexScreen.navigationOptions = () => {
+IndexScreen.navigationOptions = ({ navigation }) => {
   return {
     headerRight: () => (
       <TouchableOpacity onPress={() => navigation.navigate('Create')}>
