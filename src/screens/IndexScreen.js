6f239860fa39898c6a6f0367e8407f4a3b14673c
Travis Lamberte <travis@lamberte.com>
2023-11-09 08:27:28 -0800

    add navigation icon to index screen

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -30,6 +30,17 @@ const IndexScreen = ({ navigation }) => {
   )
 }
 
+IndexScreen.navigationOptions = () => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+        <Feather name="plus" size={30} />
+      </TouchableOpacity>
+    ),
+  };
+}
+
+
 const styles = StyleSheet.create({
   row: {
     flexDirection: 'row',
