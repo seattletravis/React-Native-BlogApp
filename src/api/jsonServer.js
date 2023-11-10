bda8b8577a9d249cf3da0a518d58416caab34bcb
Travis Lamberte <travis@lamberte.com>
2023-11-10 13:37:01 -0800

    add get method and ngrok for json db

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export default axios.create({
+    baseURL: 'https://6f4e-97-113-117-33.ngrok-free.app'
+})
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,10 @@
 import createDataContext from './createDataContext'
+import jsonServer from '../api/jsonServer'
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'get_blogposts':
+      return action.payload
     case 'edit_blogpost':
       return state.map((blogPost) => {
         return blogPost.id === action.payload.id ? action.payload : blogPost
@@ -18,6 +21,14 @@ const blogReducer = (state, action) => {
         }]
     default:
       return state
+  }
+}
+
+const getBlogPosts = dispatch => {
+  return async () => {
+    const response = await jsonServer.get('/blogposts')
+    
+    dispatch({ type: 'get_blogposts', payload: response.data })
   }
 }
 
@@ -47,6 +58,6 @@ const editBlogPost = dispatch => {
 
 export const { Context, Provider } = createDataContext(
   blogReducer, 
-  { addBlogPost, deleteBlogPost, editBlogPost },
-  [{ title: "Test Post", content: 'Test Content', id: 1 }]
+  { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts },
+  []
 )
