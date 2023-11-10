4b721b461d0f99d962c968845c9b8394816a2d0c
Travis Lamberte <travis@lamberte.com>
2023-11-10 14:43:21 -0800

    add remove to did focus listener

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -10,9 +10,14 @@ const IndexScreen = ({ navigation }) => {
   useEffect(()=>{
     getBlogPosts()
 
-    navigation.addListener('didFocus', () => {
+    const listener = navigation.addListener('didFocus', () => {
       getBlogPosts()
     })
+
+    return () => {
+      listener.remove()
+    }
+
   },[])
 
   return (
