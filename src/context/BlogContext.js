bc1d9b0448ebf4f4f9bf59140eb8d6f5b4c4a7f5
Travis Lamberte <travis@lamberte.com>
2023-11-10 08:11:09 -0800

    navigate user back after edit

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -24,7 +24,9 @@ const blogReducer = (state, action) => {
 const addBlogPost = dispatch => {
   return (title, content, callback) => {
     dispatch({ type: 'add_blogpost', payload: {title, content}})
-    callback()
+    if (callback){
+      callback()
+    }
   }
 }
 
@@ -35,12 +37,13 @@ const deleteBlogPost = dispatch => {
 }
 
 const editBlogPost = dispatch => {
-  return (id, title, content) => {
+  return (id, title, content, callback) => {
     dispatch({ type: 'edit_blogpost', payload: {id, title, content}})
+    if(callback) {
+      callback()
+    }
   }
 }
-
-
 
 export const { Context, Provider } = createDataContext(
   blogReducer, 
